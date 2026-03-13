8e5db6ba8177addadce22d1444830bb77408e22d
yoshitha33 <yoshithanunna77@gmail.com>
2026-03-13 08:17:47 +0530

    sign up bug fixed

--- backend/models/User.js
+++ backend/models/User.js
@@ -85,10 +85,11 @@ userSchema.index({ "enrolledCourses.courseId": 1 });
 // Hash password before saving
 userSchema.pre("save", async function (next) {
 	if (!this.isModified("password")) {
-		next();
+		return next();
 	}
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
+	return next();
 });
 
 // Compare password
