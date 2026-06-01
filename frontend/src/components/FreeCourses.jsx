7a6a18a984bac910b9014a3740453e2bdc471bb4
Bhargavi Tejaswi <bhargavitejaswi97@gmail.com>
2026-06-01 12:47:19 +0530

    ready to deploy

--- frontend/src/components/FreeCourses.jsx
+++ frontend/src/components/FreeCourses.jsx
@@ -20,7 +20,6 @@ export default function FreeCourses() {
 			duration: "150 Hours",
 			weeks: "4 Months",
 			thumbnail: "/Agentic_AI_DSA.png",
-			thumbnail: "./pioneerthumb.png",
 			topics: ["DSA", "ML & DL", "LangChain", "CrewAI", "LangGraph", "AI Agents", "RAG", "AutoGen"],
 			path: "/courses/agentic-ai-pioneer-program"
 		},
